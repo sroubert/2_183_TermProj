ee6c21ede68f3d9e4a1d62d5265ac4a8aee82de5
nicarons <nicarons@gmail.com>
2021-05-17 15:56:49 -0400

    added 3 dof case

--- torqueFromImpedance.m
+++ torqueFromImpedance.m
@@ -18,9 +18,9 @@ function tau = torqueFromImpedance(z,ti,p)
         dth3=z(6);
         
         th=[th1;th2;th3];
-        dth=[dth1;dth2;dth3];
+        dth=[dth1;dth2-dth1;dth3-dth2-dth1];%relative velocities
         
-        th0_t=[interp1(t,th0(:,1),ti);interp1(t,th0(:,2),ti),interp1(t,th0(:,3),ti)]; %approx th0 values at specified t
+        th0_t=[interp1(t,th0(:,1),ti);interp1(t,th0(:,2),ti);interp1(t,th0(:,3),ti)]; %approx th0 values at specified t
     end
     
     if p.dof==2
@@ -30,8 +30,8 @@ function tau = torqueFromImpedance(z,ti,p)
         dth1=z(3);
         dth2=z(4);
         
-        th=[th1;th2];
-        dth=[dth1;dth2];
+        th=[th1;th2]; 
+        dth=[dth1;dth2-dth1];%relative velocities
     
         th0_t=[interp1(t,th0(:,1),ti);interp1(t,th0(:,2),ti)]; %approx th0 values at specified t
     end
@@ -41,13 +41,20 @@ function tau = torqueFromImpedance(z,ti,p)
     
     tau=K*[th0_t-th]-B*dth;
     
-    tau_1_limit=5;
-    tau_2_limit=3;
+    tau_1_limit=50;
+    tau_2_limit=30;
+    tau_3_limit=20;
     
     if abs(tau(1))>tau_1_limit
         tau=[tau_1_limit*sign(tau(1));tau(2)];
     end
     if abs(tau(2))>tau_2_limit
         tau=[tau(1);tau_2_limit*sign(tau(2))];
+    end
+    
+    if p.dof==3
+       if abs(tau(3))>tau_3_limit 
+          tau = [tau(1);tau(2);tau_3_limit*sign(tau(3))]; 
+       end
     end
 end
