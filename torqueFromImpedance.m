f86cb913300806dda55b703679e378044236eb05
nicarons <nicarons@gmail.com>
2021-05-17 20:13:41 -0400

    Fixed an error

--- torqueFromImpedance.m
+++ torqueFromImpedance.m
@@ -18,7 +18,7 @@ function tau = torqueFromImpedance(z,ti,p)
         dth3=z(6);
         
         th=[th1;th2;th3];
-        dth=[dth1;dth2-dth1;dth3-dth2-dth1];%relative velocities
+        dth=[dth1;dth2;dth3];
         
         th0_t=[interp1(t,th0(:,1),ti);interp1(t,th0(:,2),ti);interp1(t,th0(:,3),ti)]; %approx th0 values at specified t
     end
@@ -31,7 +31,7 @@ function tau = torqueFromImpedance(z,ti,p)
         dth2=z(4);
         
         th=[th1;th2]; 
-        dth=[dth1;dth2-dth1];%relative velocities
+        dth=[dth1;dth2];
     
         th0_t=[interp1(t,th0(:,1),ti);interp1(t,th0(:,2),ti)]; %approx th0 values at specified t
     end
@@ -39,7 +39,7 @@ function tau = torqueFromImpedance(z,ti,p)
     K=p.K;
     B=p.B;   
     
-    tau=K*[th0_t-th]-B*dth;
+    tau=K*(th0_t-th)-B*dth; 
     
     tau_1_limit=50;
     tau_2_limit=30;
@@ -47,7 +47,7 @@ function tau = torqueFromImpedance(z,ti,p)
     
     if abs(tau(1))>tau_1_limit
         tau=[tau_1_limit*sign(tau(1));tau(2)];
-    end
+    end 
     if abs(tau(2))>tau_2_limit
         tau=[tau(1);tau_2_limit*sign(tau(2))];
     end
@@ -57,4 +57,5 @@ function tau = torqueFromImpedance(z,ti,p)
           tau = [tau(1);tau(2);tau_3_limit*sign(tau(3))]; 
        end
     end
+
 end
